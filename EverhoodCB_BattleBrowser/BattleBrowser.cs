dd317cd8e1438b412eaaecb84341763674519359
unknown <v8.ninja@gmail.com>
2024-01-01 16:34:23 -0500

    Created initial version of project

---
+++ EverhoodCB_BattleBrowser/BattleBrowser.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using EverhoodModding;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace EverhoodCB_BattleBrowser
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class BattleBrowser : BaseUnityPlugin
+    {
+        public const string pluginGuid = "v8ninja.everhoodcb.battlebrowser";
+        public const string pluginName = "Battle Browser (by V8_Ninja)";
+        public const string pluginVersion = "0.0.1";
+
+        private EverhoodModInstaller everhoodMI;
+
+        public void Start()
+        {
+            // Getting the Everhood Mod Installer instance
+            everhoodMI = FindObjectOfType<EverhoodModInstaller>();
+            if (everhoodMI == null)
+            {
+                Logger.LogError("Could not find EverhoodModInstaller instance!");
+            }
+
+            // Patching DLL methods
+            Harmony hrm = new Harmony(pluginGuid);
+
+            MethodInfo orgCBSUI = AccessTools.Method(typeof(EverhoodModInstaller), "CreateBattleSlotUI");
+            MethodInfo newCBSUI = AccessTools.Method(typeof(BrowserPatches), "CreateBattleSlotUI_BBPatch");
+            hrm.Patch(orgCBSUI, new HarmonyMethod(newCBSUI));
+            
+
+            // Posting A-Okay message in logging
+            if (everhoodMI != null)
+            {
+                Logger.LogInfo("Mod done loading!");
+            }
+        }
+
+        public void Update()
+        {
+            if (everhoodMI == null)
+            {
+                everhoodMI = FindObjectOfType<EverhoodModInstaller>();
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                BrowserPatches.modIndex++;
+                BrowserPatches.UpdateBattleSlotUI(everhoodMI);
+            }
+            if (Input.GetKeyDown(KeyCode.Q) && BrowserPatches.modIndex > 0)
+            {
+                BrowserPatches.modIndex--;
+                BrowserPatches.UpdateBattleSlotUI(everhoodMI);
+            }
+        }
+    }
+}
---
+++ EverhoodCB_BattleBrowser/BrowserPatches.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx.Logging;
+using EverhoodModding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EverhoodCB_BattleBrowser
+{
+    public class BrowserPatches
+    {
+        public static int modIndex = 0;
+
+        public static bool CreateBattleSlotUI_BBPatch(
+            List<EverhoodModInstaller.ModInfos> ___modInfos
+            ,List<BattleSlotUI> ___battleSlotUIs
+        )
+        {
+            
+            for (int slotNum = 0; slotNum < ___battleSlotUIs.Count; slotNum++)
+            {
+                ___battleSlotUIs[slotNum].gameObject.SetActive(false);
+            }
+            int offset = modIndex;
+            for (int modInfoNum = offset; modInfoNum < ___modInfos.Count && (modInfoNum - offset) < ___battleSlotUIs.Count; modInfoNum++)
+            {
+                ___battleSlotUIs[modInfoNum - offset].gameObject.SetActive(true);
+                ___battleSlotUIs[modInfoNum - offset].UpdateUI(___modInfos[modInfoNum]);
+            }
+
+            return false;
+        }
+
+        public static void UpdateBattleSlotUI(EverhoodModInstaller everhoodMI)
+        {
+            BindingFlags flagsPrivInst = BindingFlags.NonPublic | BindingFlags.Instance;
+            MethodInfo createBattleSlotUI = everhoodMI.GetType().GetMethod("CreateBattleSlotUI", flagsPrivInst);
+            createBattleSlotUI.Invoke(everhoodMI, new object[] { });
+        }
+    }
+}
